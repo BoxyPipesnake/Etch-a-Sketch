a2cf067d22b4a9d3d228cb0d21b4cb0a1acdd26f
Felipe Mata <felipe_motoss@hotmail.com>
2023-03-21 17:16:42 -0600

    create remove function and add  the buttons for grid selection

--- app.js
+++ app.js
@@ -5,23 +5,18 @@ function random_bg_color() {
     return "rgb(" + x + "," + y + "," + z + ")";
 }
 
-
 // https://stackoverflow.com/questions/57550082/creating-a-16x16-grid-using-javascript
+
+const grid1 = document.querySelector('#grid1');
+const grid2 = document.querySelector('#grid2');
+const grid3 = document.querySelector('#grid3');
+
+const container = document.querySelector("#container");
 
 const clearCell = document.querySelector('#clearBtn');
 const paintAgain = document.querySelector('#color');
 const clearAll = document.querySelector('#clearAll');
-
 const multiColor = document.querySelector('#multiColor');
-
-const container = document.querySelector("#container");
-
-
-container.addEventListener('mouseover', (e) => {
-    e.target.style.backgroundColor = "black";
-});
-
-
 
 function makeRows(rows, cols) {
   container.style.setProperty('--grid-rows', rows);
@@ -33,19 +28,33 @@ function makeRows(rows, cols) {
   };
 };
 
-makeRows(16, 16);
+makeRows(16, 16)
 
+function removeChild(){
+    while(container.hasChildNodes()){
+        container.removeChild(container.firstChild); // Remove the first child of list everytime until the list is null.
+    }
+}
 
+let containerElements = document.querySelectorAll('.grid-container *');
 
+grid2.addEventListener('click', ()=>{
+    removeChild();
+    makeRows(20, 20);
+    containerElements = document.querySelectorAll('.grid-container *');
+});
 
+grid3.addEventListener('click', ()=>{
+    removeChild()
+    makeRows(30, 30);
+    containerElements = document.querySelectorAll('.grid-container *');
+});
 
-
-
-
-
-
-const containerElements = document.querySelectorAll('.grid-container *');
-
+grid1.addEventListener('click', ()=>{
+    removeChild()
+    makeRows(16, 16);
+    containerElements = document.querySelectorAll('.grid-container *');
+});
 
 paintAgain.addEventListener('click', ()=>{
     container.addEventListener('mouseover', (e) => {
